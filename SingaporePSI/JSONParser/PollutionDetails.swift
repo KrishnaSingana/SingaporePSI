d9752019ead49a6a2d526feb91889bbbf2419791
Krishna Singana <krishna.singana@capgemini.com>
2019-12-11 10:10:55 +0530

    1) Integrated apple maps in project. 2) added model class for data response. 3) created annotation object for map

---
+++ SingaporePSI/JSONParser/PollutionDetails.swift
@@ -0,0 +1,78 @@
+//
+//  PollutionDetails.swift
+//  SingaporePSI
+//
+//  Created by Krishna Singana on 10/12/19.
+//  Copyright © 2019 Krishna Singana. All rights reserved.
+//
+
+import Foundation
+
+struct PollutionDetails: Codable {
+    var regionsMetadata: [PSIMetaData]
+    var items: [PSIItem]
+
+    private enum CodingKeys: String, CodingKey {
+        case regionsMetadata = "region_metadata"
+        case items = "items"
+    }
+}
+
+struct PSIMetaData: Codable {
+    var direction: String
+    var location: PSILocation
+
+    private enum CodingKeys: String, CodingKey {
+        case direction = "name"
+        case location = "label_location"
+    }
+}
+
+struct PSILocation: Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct PSIItem: Codable {
+    var timeStamp: String?
+    var updateTimeStamp: String?
+    var psiReadings: PSIReading
+
+    private enum CodingKeys: String, CodingKey {
+        case timeStamp = "timestamp"
+        case updateTimeStamp = "update_timestamp"
+        case psiReadings = "readings"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        timeStamp = try container.decodeIfPresent(String.self, forKey: .timeStamp)
+        updateTimeStamp = try container.decodeIfPresent(String.self, forKey: .updateTimeStamp)
+        psiReadings = try container.decode(PSIReading.self, forKey: .psiReadings)
+    }
+}
+
+struct PSIReading: Codable {
+    var o3_sub_index: PSISubReading
+    var pm10_twenty_four_hourly: PSISubReading
+    var pm10_sub_index: PSISubReading
+    var co_sub_index: PSISubReading
+    var pm25_twenty_four_hourly: PSISubReading
+    var so2_sub_index: PSISubReading
+    var co_eight_hour_max: PSISubReading
+    var no2_one_hour_max: PSISubReading
+    var so2_twenty_four_hourly: PSISubReading
+    var pm25_sub_index: PSISubReading
+    var psi_twenty_four_hourly: PSISubReading
+    var o3_eight_hour_max: PSISubReading
+}
+
+struct PSISubReading: Codable {
+    var west: Double
+    var national: Double
+    var east: Double
+    var central: Double
+    var south: Double
+    var north: Double
+}
---
+++ SingaporePSI/ViewControllers/PSIViewController.swift
@@ -0,0 +1,115 @@
+//
+//  PSIViewController.swift
+//  SingaporePSI
+//
+//  Created by Krishna Singana on 10/12/19.
+//  Copyright © 2019 Krishna Singana. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PSIViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    fileprivate let regionRadius: CLLocationDistance = 66000
+    fileprivate let baseURL = "https://api.data.gov.sg/v1/environment/psi"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // set initial location to Singapore
+        let initialLocationOnMap = CLLocation(latitude: 1.35735, longitude: 103.82)
+        self.centerMapOnLocation(location: initialLocationOnMap)
+
+        mapView.delegate = self
+
+        self.getPollutionDetailsFor(date: "2019-12-10", dateTime: nil)
+    }
+
+    fileprivate func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+}
+
+extension PSIViewController: MKMapViewDelegate {
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? PollutionAnnotation else { return nil }
+        let annotationViewIdentifier = "annotationViewIdentifier"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationViewIdentifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: annotationViewIdentifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+        }
+        return view
+    }
+}
+
+// MARK: - Api calls
+extension PSIViewController {
+
+    //This method is used for generating pollutionApi URL String
+    private func generatePollutionApiURL(with date: String, dateTime: String?) -> String {
+        var endPointURL = "?date=\(date)"
+        if let dateTimeValue = dateTime {
+            endPointURL.append("&date_time=\(dateTimeValue)")
+        }
+        let hostURL = "\(baseURL)\(endPointURL)"
+        return hostURL
+    }
+
+    //This method is used for getting pollutionApi URLRequest
+    func getPollutionApiRequest(with date: String, dateTime: String?) -> URLRequest? {
+        if let request: URLRequest = self.baseRequestForURL(url: self.generatePollutionApiURL(with: date, dateTime: dateTime), method: "GET") {
+            return request
+        }
+        return nil
+    }
+
+    //This method is used to provide basic common information required while creating URLRequest.
+    private func baseRequestForURL(url: String, contentType: String? = nil, httpBody: [String: Any]? = nil, method: String) -> URLRequest? {
+        if let url = URL(string: url) {
+            let request = NSMutableURLRequest(url: url as URL)
+            request.httpMethod = method
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            if let body = httpBody as? [String: String], method == "POST" || method == "PUT" {
+                let jsonData = try? JSONSerialization.data(withJSONObject: body)
+                request.httpBody = jsonData
+            }
+            return request as URLRequest
+        } else {
+            return nil
+        }
+    }
+
+    //This method is used for getting pollution details from API
+    fileprivate func getPollutionDetailsFor(date: String, dateTime: String?) {
+        let defaultSession = URLSession(configuration: .default)
+
+        let dataTask = defaultSession.dataTask(with: self.getPollutionApiRequest(with: date, dateTime: dateTime)!) { data, response, error in
+            if (response as? HTTPURLResponse)?.statusCode == 200 {
+                guard let pollutionData = data else { return }
+                do {
+                    let pollutionDetails = try JSONDecoder().decode(PollutionDetails.self, from: pollutionData)
+
+                } catch {
+                    print("JSON Data Parsing Error : \(error)")
+                }
+
+            } else {
+
+            }
+        }
+        dataTask.resume()
+    }
+}
