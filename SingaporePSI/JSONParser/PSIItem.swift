93e9ccbd3f4a971c5e2870a1346e6e83a45937b3
Krishna Singana <krishna.singana@capgemini.com>
2019-12-13 11:13:27 +0530

    Created new file for each JSON Parsing model.

---
+++ SingaporePSI/JSONParser/PSIItem.swift
@@ -0,0 +1,29 @@
+//
+//  PSIItem.swift
+//  SingaporePSI
+//
+//  Created by Krishna Singana on 13/12/19.
+//  Copyright © 2019 Krishna Singana. All rights reserved.
+//
+
+import Foundation
+
+struct PSIItem: Codable {
+    var timeStamp: String?
+    var updateTimeStamp: String?
+    var psiReadings: PSIReading
+
+    private enum CodingKeys: String, CodingKey {
+        case timeStamp = "timestamp"
+        case updateTimeStamp = "update_timestamp"
+        case psiReadings = "readings"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        timeStamp = try container.decodeIfPresent(String.self, forKey: .timeStamp)
+        updateTimeStamp = try container.decodeIfPresent(String.self, forKey: .updateTimeStamp)
+        psiReadings = try container.decode(PSIReading.self, forKey: .psiReadings)
+    }
+}
